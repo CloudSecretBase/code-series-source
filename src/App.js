1d1fe09badaee1197ab3b05e14c0af9f64497fd6
xiaomo <suzukaze.hazuki2020@gmail.com>
2022-01-14 21:59:36 +0900

    chore: deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello AWS V4
+          Hello AWS CodeDeploy
         </a>
       </header>
     </div>
